af6f32bf568a961b82e1e76f4319a3cab46999d0
doncibo <qajonesx47@gmail.com>
2021-03-31 12:12:20 -0500

    Added ssl and database fix

--- app.js
+++ app.js
@@ -11,6 +11,24 @@ const { ValidationError } = require("sequelize");
 const { AuthenticationError } = require('./routes/util/auth');
 
 const app = express();
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
